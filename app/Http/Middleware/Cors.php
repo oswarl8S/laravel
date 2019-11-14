d239030c26c8ac5d5cdf9d5e1d7a0ae283a1b290
Oswaldo Ruiz Lopez <oswarl8s@gmail.com>
2019-11-14 12:12:39 -0600

    ok

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,6 +15,23 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
+        /*
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            if (!headers_sent()) {
+                header('Access-Control-Allow-Origin: *');
+            }
+        }
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set(
+            'Access-Control-Allow-Headers',
+            'Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since'
+        );
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response->headers->set('Access-Control-Allow-Methods', '*');
+        $response->headers->set('Access-Control-Allow-Headers', '*');
+        return $response;
+        */
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
